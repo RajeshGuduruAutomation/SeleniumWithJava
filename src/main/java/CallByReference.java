2e5ade570f154f0786bffcb8963c7fad327963ab
rhguduru <rajesh.guduru@sumtotalsystems.com>
2021-04-28 15:37:33 +0530

    Java programs

--- src/main/java/CallByReference.java
+++ src/main/java/CallByReference.java
@@ -1,8 +1,9 @@
 import java.lang.reflect.InvocationTargetException;
 
 public class CallByReference {
-    int x;
-    public CallByReference() {
+    public int x;
+    private static CallByReference getInstance;
+    private CallByReference() {
 
     }
     CallByReference(int x) {
@@ -17,19 +18,38 @@ public class CallByReference {
 //        x = 70;
 //        System.out.println(x);
     }
+
+    public static CallByReference getInstance() {
+        if(CallByReference.getInstance != null || CallByReference.getInstance == null) {
+            System.out.println(CallByReference.getInstance);
+        }
+        if(CallByReference.getInstance == null) {
+
+            getInstance = new CallByReference();
+            System.out.println(CallByReference.getInstance);
+        }
+        return getInstance;
+    }
+
+
+}
+
+class refer {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, CloneNotSupportedException {
-        CallByReference cbr = (CallByReference) Class.forName("CallByReference").getDeclaredConstructor().newInstance();
+        //CallByReference cbr = (CallByReference) Class.forName("CallByReference").getDeclaredConstructor().newInstance();
+        CallByReference cbr = CallByReference.getInstance();
         System.out.println(cbr.x);
         cbr.change(50);
         System.out.println(cbr.x);
-        CallByReference cbr1 = new CallByReference();
+        cbr.x = 30;
+        System.out.println(cbr.x);
+        CallByReference cbr1 = CallByReference.getInstance();
         System.out.println(cbr1.x);
         cbr1.change(60);
         System.out.println(cbr.x);
         System.out.println(cbr1.x);
     }
 }
-
 
 
 
